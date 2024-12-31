90f03633180ac2c7b0f080dcbdc16e3569f458e2
Charles Channon <channon.charles@gmail.com>
2024-12-31 07:53:28 -0500

    cleanup settings and gitignore

--- ActivateFlows/obj/Debug/net6.0/ActivateFlows.AssemblyInfo.cs
+++ ActivateFlows/obj/Debug/net6.0/ActivateFlows.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActivateFlows")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a50f7a7f526552803c057a4176bd3c235e368f87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+664a84c3639159bfc640fa7327909f9b7f6292af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActivateFlows")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActivateFlows")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
